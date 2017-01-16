35b60db1f31953942c354c91d263e1bba0f66328
Jan Wilken Dörrie <dojan@ethz.ch>
2017-01-16 23:06:40 +0100

    C++ Wrapper Around Hand Index Library

---
+++ jdoerrie/hand_indexer.cc
@@ -0,0 +1,58 @@
+#include "hand_indexer.h"
+
+#include <numeric>
+
+#include "card_set.h"
+
+HandIndexer::HandIndexer(const std::vector<uint8_t>& cards_per_round)
+    : cards_per_round_{cards_per_round} {
+  hand_indexer_init(cards_per_round_.size(), cards_per_round_.data(),
+                    &hand_indexer_);
+}
+
+size_t HandIndexer::size() const {
+  return hand_indexer_size(&hand_indexer_, cards_per_round_.size() - 1);
+}
+
+size_t HandIndexer::IndexHands(const std::vector<CardSet>& hands) const {
+  std::vector<uint8_t> cards;
+  for (const auto& hand : hands) {
+    for (const auto& card : hand.ToCards()) {
+      auto suit = std::string(SUIT_TO_CHAR).find(card.suit().ToChar());
+      auto rank = std::string(RANK_TO_CHAR).find(card.rank().ToChar());
+      cards.push_back(deck_make_card(suit, rank));
+    }
+  }
+
+  return hand_index_last(&hand_indexer_, cards.data());
+}
+
+std::vector<CardSet> HandIndexer::UnindexHands(size_t index) const {
+  std::vector<CardSet> hands;
+  auto rounds = hand_indexer_.rounds;
+  std::vector<uint8_t> cards(std::accumulate(std::begin(cards_per_round_),
+                                             std::end(cards_per_round_), 0));
+  if (!hand_unindex(&hand_indexer_, rounds - 1, index, cards.data())) {
+    return {};
+  }
+
+  CardSet hand;
+  auto round = 0;
+  for (auto card : cards) {
+    char rank = RANK_TO_CHAR[deck_get_rank(card)];
+    char suit = SUIT_TO_CHAR[deck_get_suit(card)];
+    hand.Add({Rank{rank}, Suit{suit}});
+
+    if (hand.size() == cards_per_round_[round]) {
+      hands.push_back(std::move(hand));
+      ++round;
+      hand = {};
+    }
+  }
+
+  return hands;
+}
+
+HandIndexer::~HandIndexer() {
+  hand_indexer_free(&hand_indexer_);
+}
---
+++ jdoerrie/hand_indexer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+extern "C" {
+#define _Bool bool
+#include "lib/hand-isomorphism/src/hand_index.h"
+}
+
+#include <vector>
+
+class CardSet;
+
+class HandIndexer {
+ public:
+  HandIndexer(const std::vector<uint8_t>& cards_per_round);
+  ~HandIndexer();
+
+  size_t size() const;
+  size_t IndexHands(const std::vector<CardSet>& hands) const;
+  std::vector<CardSet> UnindexHands(size_t index) const;
+
+ private:
+  hand_indexer_t hand_indexer_;
+  std::vector<uint8_t> cards_per_round_;
+};
